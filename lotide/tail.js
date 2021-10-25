0fa18e66aa362bf645f1eaa11201a3bb2e63eb13
fddiashok <fddiashok@gmail.com>
2021-10-25 22:41:56 +0000

    Custom library function to access tail elements on array

---
+++ lotide/tail.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`${actual} == ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`${actual} != ${expected}`);
+  }
+};
+const tail = function(array) {
+  let newArray = [];
+  for (let i = 1; i < array.length; i++) {
+    newArray.push(array[i]);
+  }
+  return (newArray);
+};
+// Test Case:Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!*/
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
