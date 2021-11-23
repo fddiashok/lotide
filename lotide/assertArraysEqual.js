1d16b6ecf5f1706a06fc83472b9ec2db3846a56a
fddiashok <fddiashok@gmail.com>
2021-11-23 10:07:16 -0500

    Final Commit to assertArraysEqual

--- lotide/assertArraysEqual.js
+++ lotide/assertArraysEqual.js
@@ -1,17 +1,26 @@
-function eqArrays(arr1 , arr2) {
+const assertEqual = function (actual, expected) {
+  if (actual.length === expected.length) {
+    console.log(`Are arrays equal? : ${eqArrays(actual,expected)}`);
+  } else if (actual.length !== expected.length) {
+    console.log(`Are arrays unequal: True`);
+  }
+};
+
+function eqArrays(arr1, arr2) {
   if (arr1.length != arr2.length)
-    return "Arrays are not equal";
+    return false;
   else {
     // comapring each element of array
-    for (var i = 0; i < arr1.length; i++)
+    for (var i = 0; i < arr1.length; i++) {
       if (arr1[i] !== arr2[i]) {
-        return "Arrays are not equal";
+        return false;
       }
+    }
+    return true;
   }
-  return "Arrays are equal";
 }
-console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => should PASS
-console.log(eqArrays([1, 2, 3], [1, 2, 3, 4])); //FAIL
-console.log(eqArrays([1, 2, 3], [3, 2, 3])); //FAIL
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+  assertEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+  assertEqual([1, 2, 3], [1, 2, 3, 4]); //FAIL
+  assertEqual([1, 2, 3], [3, 2, 3]); //FAIL
+  assertEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+  assertEqual(["1", "2", "3"], ["1", "2", 3]); // => false
