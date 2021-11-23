a018dd39e1aae41e0945e13faed2ff9464e8807f
fddiashok <fddiashok@gmail.com>
2021-11-23 14:42:52 -0500

    Final commit to without function

--- lotide/without.js
+++ lotide/without.js
@@ -1,19 +1,44 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} == ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`Assertion Failed: ${actual} != ${expected}`);
+const assertArraysEqual = function (actual, expected) {
+  if (actual.length === expected.length) {
+    console.log(`${eqArrays(actual,expected)}`);
+  } else if (actual.length !== expected.length) {
+    console.log(`false`);
   }
 };
-function eqArrays(a, b) {
-  if (a.length != b.length)
+
+function eqArrays(arr1, arr2) {
+  if (arr1.length != arr2.length)
     return false;
   else {
     // comapring each element of array
-    for (var i = 0; i < a.length; i++)
-      if (a[i] != b[i]) {
+    for (var i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
         return false;
       }
+    }
+    return true;
   }
-  return true;
 }
+const without = function (source, itemsToRemove) {
+  const newArray = [];
+  // if (array1.length >= array2.length) {
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      newArray.push(source[i])
+    }
+  }
+  console.log(newArray);
+
+};
+
+//Test Cases 
+without([1, 2, 3], [1]) // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]) // => ["1", "2"]
+without(["1", "2", "3"], [1, "2", "3", "4"]) // => ["1"]
+without(["1", "2", "3","4"], ["1", "2", "3"]) // => ["4"]
+without(["1", "2", "3","4"], []) // => ["1", "2", "3", "4"]
+
+// Test Cases to verify if any changes made to source array or not
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); 
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
