bd6816f5cf7c1e28ee1d8ace6f07d72bbd68556d
fddiashok <fddiashok@gmail.com>
2021-11-24 14:16:09 -0500

    Changed return type to an Object

--- lotide/countLetters.js
+++ lotide/countLetters.js
@@ -5,30 +5,54 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑Assertion Failed: ${actual} != ${expected}`);
   }
 };
+
+
+const converStringToArray=function(string){
+  if(string=''){
+    
+  }
+  return (string.split(' ').join('')).split('');
+}
+
+const countLetters=function (string) {
+  const input= converStringToArray(string);
+  // console.log(input);
+  const result={};
+  for(let item of input){
+    if(result[item]){
+      result[item]+=1;
+    }
+    else result[item]=1
+  }
+  return result;
+}
+//Test Cases
+console.log(countLetters("lighthouse in the house"));
+console.log(countLetters("ashok"));
+console.log(countLetters("the house"));
+console.log(countLetters("aaaa bc cbad"));
+console.log(countLetters(""));
+
+/*Returning Number of occurences as primitive data type
 function count_occur(str) {
-  if( str.length == 0 ) {
+  const noSpaces=str.split(' ').join('');
+  if( noSpaces.length == 0 ) {
     console.log("Invalid string")
     return;
   }
 
-  for(var i = 0 ;i < str.length ;i++) {
+  for(var i = 0 ;i < noSpaces.length ;i++) {
     var count = 0;
-    for(var j = 0 ;j < str.length ;j++) {
-      if(str[i] == str[j] && i > j) {
+    for(var j = 0 ;j < noSpaces.length ;j++) {
+      if(noSpaces[i] == noSpaces[j] && i > j) {
        break;
       }
-      if(str[i] == str[j]) {
+      if(noSpaces[i] == noSpaces[j]) {
         count++;
       }
     }
     if( count > 0)
-    console.log(`${str[i]} occurs ${count} times`);
+    console.log(`${noSpaces[i]} occurs ${count} times`);
   }
  
-}
- 
-// let test_str = "LHLKFGH";
-count_occur("LHLKFGH");
-count_occur("AMMANO");
-count_occur("EDDIE");
-count_occur("JOHN");
+}*/
