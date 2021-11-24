9afa44ac80779f757cc292ccf0088ea5aae297c0
fddiashok <fddiashok@gmail.com>
2021-11-24 18:08:34 -0500

    intial commit

--- lotide/assertObjectsEqual.js
+++ lotide/assertObjectsEqual.js
@@ -1,32 +1,63 @@
-function eqObjects(object1, object2) {
-  if (Object.keys(object1).length !== Object.keys(object2).length) {
-    return false;
+const assertObjectsEqual = function (actual, expected) {
+  const inspect = require('util').inspect; // <= add this line
+  if (eqObjects(actual,expected)) {
+    console.log(`✅✅✅Assertion Passed: ${inspect(actual)} == ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${inspect(actual)} != ${inspect(expected)}`);
   }
-
-  const obj1keys = Object.keys(object1);
-  for (var i = 0; i < obj1keys.length; i++) {
-    let key = obj1keys[i];
-    if (Array.isArray(object1[key])) {
-      return eqArrays(object1[key], object2[key]);
-    } else if (object1[key] !== object2[key]) {
-      return false;
-    }
-  }
-  return true;
 };
 
+//Are Arrays Equal?
 function eqArrays(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
+  if (arr1.length != arr2.length)
     return false;
-  }
-  for (var i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+  else {
+    // comapring each element of array
+    for (var i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
     }
+    return true;
   }
-  return true;
+}
+
+// Returns true if both objects have identical keys with identical values.
+// Otherwise you get back a big fat false!
+const eqObjects = function (object1, object2) {
+  const keys1 = Object.keys(object1)
+  const keys2 = Object.keys(object2);
+
+
+  if (keys1.length == keys2.length) {
+    for (let item of keys1) {
+      //Check if arrays are equal or not
+      if (Array.isArray(object1[item]) && Array.isArray(object2[item])) {
+        return eqArrays(object1[item], object2[item]);
+      }
+      else if (object1[item] !== object2[item])
+        return false
+    }
+    return true
+
+  }
+  else return false;
 };
 
+console.log("ASSERTION RESULTS FOR PRIMITIVE DATA TYPES---------------");
+//ASSERTIONS FOR PRIMITIVE VLAUES
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertObjectsEqual(ab, ba); // => true
 
-console.log(eqObjects({ a: "1", b: "2" }, { b: "2", a: "1" }));
-console.log(eqObjects({ b: "2", a: "1" }, { a: "1", b: "2", c: "3" }));
+const abc = { a: "1", b: "2", c: "3" };
+assertObjectsEqual(ab, abc); // => false
+
+console.log("ASSERTION RESULTS FOR ARRAYS-----------")
+// ASSERTIONS FOR ARRAY VALUES
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertObjectsEqual(cd, dc); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(cd, cd2); // => false*/
