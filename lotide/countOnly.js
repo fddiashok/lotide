b75d96d1cb2e52c069285ab91df73e9c8815bc32
fddiashok <fddiashok@gmail.com>
2021-11-23 20:50:05 -0500

    added assertion test case

--- lotide/countOnly.js
+++ lotide/countOnly.js
@@ -1,8 +1,8 @@
 const assertEqual = function (actual, expected) {
   if (actual === expected) {
-    console.log(`🛑🛑🛑Assertion Passed: ${actual} == ${expected}`);
+    console.log(`✅✅✅Assertion Passed: ${actual} == ${expected}`);
   } else if (actual !== expected) {
-    console.log(`✅✅✅Assertion Failed: ${actual} != ${expected}`);
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} != ${expected}`);
   }
 };
 // allItems: an array of strings that we need to look through
@@ -39,4 +39,4 @@ const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": t
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
 assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
+assertEqual(result1["Agouhanna"], undefined);*/
