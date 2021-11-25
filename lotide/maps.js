1b2ba36e81c6a01ab7140e0d254f7b233466c523
fddiashok <fddiashok@gmail.com>
2021-11-25 15:04:03 -0500

    Added Assertions, call back functions

--- lotide/maps.js
+++ lotide/maps.js
@@ -1,16 +1,39 @@
-const words = ["ground", "control", "to", "major", "tom"];
-const map = function(array  ,callback) {
+const assertArraysEqual = function (actual, expected) {
+  if (actual.length === expected.length) {
+    console.log(`✅✅✅Assertion Passed : ${eqArrays(actual,expected)}`);
+  } else if (actual.length !== expected.length) {
+    console.log(`🛑🛑🛑Assertion Failed: ${eqArrays(actual,expected)}`);
+  }
+};
 
-  // console.log('array: ', array);
-  // console.log('callback: ', callback);
-  const results = [];
+function eqArrays(arr1, arr2) {
+  if (arr1.length != arr2.length)
+    return false;
+  else {
+    // comapring each element of array
+    for (var i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+const map = function (array, callback) {
+  let results = [];
   for (let item of array) {
     results.push(callback(item));
-    // console.log('item BEFORE: ', item);
-    // console.log('item AFTER: ', callback(item));
-    // console.log('---');
   }
+  console.log(results);
   return results;
 }
-const results1 = map(words, word => word[0]);
-console.log(results1);
+
+
+const numbers=[2,4,6,8,1,3,5]
+assertArraysEqual(map(numbers,item=> item*2),[4,8,12,16,2,6,10])
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+assertArraysEqual(map(words,item=> item[0]),['g','c','t','m','t']);
+
