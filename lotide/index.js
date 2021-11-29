e7c0fc322c2fb6d8007d529bdda3f0efe9490657
fddiashok <fddiashok@gmail.com>
2021-11-29 16:27:48 -0500

    Used Object Shorthand to clean code

--- lotide/index.js
+++ lotide/index.js
@@ -8,26 +8,24 @@ const countOnly =require('./countOnly');
 const eqArrays=require('./eqArrays');
 const findKey =require('./findKey');
 const findKeyByValue=require('./findKeyByValue');
-const letterPositions =require("./letterPositions");
+const letterPositions =require('./letterPositions');
 const maps=require('./maps');
 const takeUntil=require('./takeUntil');
 const without= require('./without');
 
 module.exports = {
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  assertArraysEqual:assertArraysEqual,
-  assertEqual: assertEqual,
-  countLetters: countLetters,
-  countOnly:countOnly,
-  eqArrays:eqArrays,
-  findKey:findKey,
-  findKeyByValue:findKeyByValue,
-  letterPositions:letterPositions,
-  maps:maps,
-  takeUntil:takeUntil,
-  without:without
-  
-
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  maps,
+  takeUntil,
+  without
 };
