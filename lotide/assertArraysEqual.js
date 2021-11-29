08e70eaed29d9235f1bde219f5959806813eef0f
fddiashok <fddiashok@gmail.com>
2021-11-29 14:34:27 -0500

    Refactored and debugged function

--- lotide/assertArraysEqual.js
+++ lotide/assertArraysEqual.js
@@ -1,26 +1,11 @@
+const eqArrays= require('./eqArrays');
 const assertArraysEqual = function (actual, expected) {
-  if (eqArray(actual, expected)) {
-    console.log(`✅✅✅Assertion Passed: ${actual}=${expected}`);
+  let result = eqArrays(actual,expected);
+  if (result) {
+    console.log(`✅✅✅Assertion Passed: [${actual}]=[${expected}]`);
   } else
-    console.log(`🛑🛑🛑Assertion Failed: ${actual} != ${expected}`);
+    console.log(`🛑🛑🛑Assertion Failed: [${actual}] != [${expected}]`);
 
 };
 
-function eqArrays(arr1, arr2) {
-  if (arr1.length != arr2.length)
-    return false;
-  else {
-    // comapring each element of array
-    for (var i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-}
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]); //FAIL
-assertArraysEqual([1, 2, 3], [3, 2, 3]); //FAIL
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+module.exports= assertArraysEqual;
